7b0739c7f500a2323413890f2a1a39199c452adb
vtthientma <vtthien@tma.com.vn>
2022-11-20 18:59:34 +0700

    Modify guess_the_number.py

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,13 +1,16 @@
+"""
+Function to guess the number from user
+"""
 import random
 number = random.randint(1, 10)
 
 player_name = input("Hello, what is your name? ")
-number_of_guesses = 0
-print('I\'m glad to meet you! {} \nLet\'s play a game with you, I will think a number between 1 and 10 then you will guess, alright? \nDon\'t forget! You have only 3 chances so guess:'.format(player_name))
+NUMBER_OF_GUESSES = 0
+print(f'I\'m glad to meet you! {player_name} \nLet\'s play a game with you, I will think a number between 1 and 10 then you will guess, alright? \nDon\'t forget! You have only 3 chances so guess:')
 
-while number_of_guesses < 3:
+while NUMBER_OF_GUESSES < 3:
     guess = int(input())
-    number_of_guesses += 1
+    NUMBER_OF_GUESSES += 1
     if guess < number:
         print('Your estimate is too low, go up a little!')
     if guess > number:
@@ -15,6 +18,6 @@ while number_of_guesses < 3:
     if guess == number:
         break
 if guess == number:
-    print( 'Congratulations {}, you guessed the number in {} tries!'.format(player_name, number_of_guesses))
+    print(f'Congratulations {player_name}, you guessed the number in {NUMBER_OF_GUESSES} tries!')
 else:
-    print('Close but no cigar, you couldn\'t guess the number. \nWell, the number was {}.'.format(number))
+    print(f'Close but no cigar, you couldn\'t guess the number. \nWell, the number was {number}.')
