2a1004a432e4015711c6f59983009836f2b2def7
vtthientma <vtthien@tma.com.vn>
2022-11-26 17:51:32 +0700

    Modify docker workflow

--- guess_the_number.py
+++ guess_the_number.py
@@ -2,22 +2,22 @@
 Function to guess the number from user
 """
 import random
-number = random.randint(1, 10)
+NUMBER = random.randint(1, 10)
 
-player_name = input("Hello, what is your name? ")
+PLAYER_NAME = input("Hello, what is your name? ")
 NUMBER_OF_GUESSES = 0
-print(f'I\'m glad to meet you! {player_name} \nLet\'s play a game with you, I will think a number between 1 and 10 then you will guess, alright? \nDon\'t forget! You have only 3 chances so guess:')
+print(f'I\'m glad to meet you! {PLAYER_NAME} \nLet\'s play a game with you, I will think a number between 1 and 10 then you will guess, alright? \nDon\'t forget! You have only 3 chances so guess:')
 
 while NUMBER_OF_GUESSES < 3:
-    guess = int(input())
+    GUESS = int(input())
     NUMBER_OF_GUESSES += 1
-    if guess < number:
+    if GUESS < NUMBER:
         print('Your estimate is too low, go up a little!')
-    if guess > number:
+    if GUESS > NUMBER:
         print('Your estimate is too high, go down a bit!')
-    if guess == number:
+    if GUESS == NUMBER:
         break
-if guess == number:
-    print(f'Congratulations {player_name}, you guessed the number in {NUMBER_OF_GUESSES} tries!')
+if GUESS == NUMBER:
+    print(f"Congratulations {PLAYER_NAME}, you guessed the number in {NUMBER_OF_GUESSES} tries!")
 else:
-    print(f'Close but no cigar, you couldn\'t guess the number. \nWell, the number was {number}.')
+    print(f'Close but no cigar, you couldn\'t guess the number. \nWell, the number was {NUMBER}.')
